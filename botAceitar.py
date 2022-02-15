9d210070eec294c798a89642bf7fdfea5e7f576d
Alisson <88199064+AlissonAnjosGit@users.noreply.github.com>
2022-02-15 10:39:05 -0300

    Update botAceitar.py

--- botAceitar.py
+++ botAceitar.py
@@ -15,8 +15,12 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 # laco que mantem o programa rodando ate o usuario apertar 'm'
-while keyboard.is_pressed('m') == False:
-    if pyautogui.locateOnScreen('aceitar.png', confidence = conf) != None:
-        botao = pyautogui.locateOnScreen('aceitar.png', grayscale = True, confidence = conf)
-        click(botao.left + int(botao.width/2), botao.top + int(botao.height/2)) #o calculo e so para achar o meio do botao
-        time.sleep(1)
+while keyboard.is_pressed('x') == False:
+    try:
+        if pyautogui.locateOnScreen('aceitar.png', confidence = conf) != None:
+            botao = pyautogui.locateOnScreen('aceitar.png', grayscale = True, confidence = conf)
+            click(botao.left + int(botao.width/2), botao.top + int(botao.height/2)) #o calculo e so para achar o meio do botao
+            time.sleep(1)
+
+    except OSError as error:
+        continue
